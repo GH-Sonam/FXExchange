b5f24f63830b242ac44001715d12ce6ea9564bb2
GH-Sonam <64680672+GH-Sonam@users.noreply.github.com>
2022-06-30 20:09:21 +0530

    Add files via upload

---
+++ FxExchange.Test/FxExchangeTest.cs
@@ -0,0 +1,32 @@
+using FXExchange.Interfaces;
+using Moq;
+using System;
+using Xunit;
+
+namespace FxExchange.Test
+{
+    public class FxExchangeTest
+    {
+        private readonly Mock<IFxExchangeCalculator> fxExchangeCalculatorService;
+
+        private readonly Mock<IFxExchangeValidator> fxExchangeValidatorService;
+
+        public FxExchangeTest()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        { 
+            //Arrange
+
+
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
---
+++ FxExchange.Test/FxExchangeValidatorTest.cs
@@ -0,0 +1,119 @@
+﻿using FXExchange;
+using FXExchange.Helper;
+using FXExchange.Interfaces;
+using FXExchange.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FxExchange.Test
+{
+    public class FxExchangeValidatorTest
+    {
+        Mock<IServiceProvider> mockServiceProvider = new Mock<IServiceProvider>();
+        Mock<IServiceScope> mockServiceScope = new Mock<IServiceScope>();
+        Mock<IFxExchangeValidator> fxValidator = new Mock<IFxExchangeValidator>();
+
+        private readonly FxExchangeValidator fxExchangeValidator;
+        Dictionary<string, RateExchangeModel> exchangeRateList;
+
+        public FxExchangeValidatorTest()
+        {
+            mockServiceScope.SetupGet<IServiceProvider>(s => s.ServiceProvider).Returns(mockServiceProvider.Object);
+
+            var serviceScopeFactoryMock = new Mock<IServiceScopeFactory>();
+            serviceScopeFactoryMock.Setup(s => s.CreateScope()).Returns(mockServiceScope.Object);
+            mockServiceProvider.Setup(s => s.GetService(typeof(IFxExchangeValidator))).Returns(fxValidator.Object);
+            fxExchangeValidator = new FxExchangeValidator();
+            exchangeRateList = RateExchange.GetExchangeRate();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            string input = "EUR/DKK 1";
+            ErrorList errorList = new ErrorList(); ;
+
+            //Act
+            ExchangeRequest result = fxExchangeValidator.ValidateRequest(input, exchangeRateList, errorList);
+
+            //Assert
+            Assert.True(string.IsNullOrWhiteSpace(errorList.ErrorMessage));
+
+            Assert.Equal(result.InputQuantity.ToString(),"1");
+            Assert.Equal(result.SourceCurrency.ToString(), "EUR");
+            Assert.Equal(result.TargetCurrency.ToString(), "DKK");
+        }
+
+        public void Sample_Success_Case()
+        {
+            //Arrange
+            string input = "EUR/DKK 1";
+            ErrorList errorList = new ErrorList(); ;
+
+            //Act
+            ExchangeRequest result = fxExchangeValidator.ValidateRequest(input, exchangeRateList, errorList);
+
+            //Assert
+            Assert.True(string.IsNullOrWhiteSpace(errorList.ErrorMessage));
+
+            Assert.Equal(result.InputQuantity.ToString(), "1");
+            Assert.Equal(result.SourceCurrency.ToString(), "EUR");
+            Assert.Equal(result.TargetCurrency.ToString(), "DKK");
+        }
+
+        [Fact]
+        public void EmptyRequest_Failure_Case()
+        {
+            //Arrange
+            string input = "";
+            ErrorList errorList = new ErrorList(); ;
+
+            //Act
+            ExchangeRequest result = fxExchangeValidator.ValidateRequest(input, exchangeRateList, errorList);
+
+            //Assert
+            Assert.True(!string.IsNullOrWhiteSpace(errorList.ErrorMessage));
+
+            Assert.Equal(errorList.ErrorMessage, Constants.EmptyRequest);
+        }
+
+        [Fact]
+        public void InvalidRequest_Failure_Case()
+        {
+            //Arrange
+            string input = "EUR 1";
+            ErrorList errorList = new ErrorList(); ;
+
+            //Act
+            ExchangeRequest result = fxExchangeValidator.ValidateRequest(input, exchangeRateList, errorList);
+
+            //Assert
+            //Assert
+            Assert.True(!string.IsNullOrWhiteSpace(errorList.ErrorMessage));
+
+            Assert.Equal(errorList.ErrorMessage, Constants.InvalidRequest);
+        }
+
+        [Fact]
+        public void InvalidISoCode_Failure_Case()
+        {
+            //Arrange
+            string input = "EUR/INR 1";
+            ErrorList errorList = new ErrorList(); ;
+
+            //Act
+            ExchangeRequest result = fxExchangeValidator.ValidateRequest(input, exchangeRateList, errorList);
+
+            //Assert
+            //Assert
+            Assert.True(!string.IsNullOrWhiteSpace(errorList.ErrorMessage));
+
+            Assert.Equal(errorList.ErrorMessage, Constants.NotAvailableISO);
+        }
+    }
+}
